8b612e4dcc76f73348df609ac39aa1d02d7e7795
Akshara <aksharagollapalli@gmail.com>
2021-06-03 13:03:47 -0400

    abstract program

---
+++ src/Bhojreddy.java
@@ -0,0 +1,16 @@
+package com.demo.oops.encapsulation;
+
+public class Bhojreddy {
+
+}
+//concrete method
+public int studentName() {
+	return studentId;
+}
+
+
+ //abstarct method
+abstract String getStudentDetails();
+
+}
+ 
