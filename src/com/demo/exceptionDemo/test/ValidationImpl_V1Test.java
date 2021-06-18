e955d8986e61e7140ec1a7213d53dc5d35061a09
Anu Dharkar <anumig@gmail.com>
2021-06-18 11:50:31 -0500

    Full validation exception handling

---
+++ src/com/demo/exceptionDemo/test/ValidationImpl_V1Test.java
@@ -0,0 +1,27 @@
+package com.demo.exceptionDemo.test;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.demo.exceptionDemo.EmployeeData;
+import com.demo.exceptionDemo.ErrorInfo;
+import com.demo.exceptionDemo.ValidationException;
+import com.demo.exceptionDemo.ValidationImpl;
+import com.demo.exceptionDemo.ValidationImpl_v1;
+
+public class ValidationImpl_V1Test {
+	ValidationImpl_v1 impl =null;
+	@Test
+	public void testvalidateEmployeeData_id() throws ValidationException {
+		EmployeeData data = new EmployeeData();
+		
+		data.setId(-1);
+		data.setName("chandu");
+		data.setSsn("Chandu123");
+		impl = new ValidationImpl_v1(data);
+		List<ErrorInfo> errorList =impl.validateEmployeeData();
+		System.out.println(errorList);
+	}
+	
+}
